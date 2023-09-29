28c61ffa1c65d409dea5a67808b0e0afc13850b1
Jairo <emmanuelmo.ti19@utsjr.edu.mx>
2023-09-28 18:16:33 -0600

    register hecho

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,10 +4,10 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    _ application: UIApplication,
+    _ application: UIApplication
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -6,7 +6,7 @@ class MainFlutterWindow: NSWindow {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    self.setFrame(windowFrame display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
